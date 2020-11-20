c99e9a28622c0fc898a68ae158ab296f2539ca9b
bogoblin <bogoblin@gmail.com>
2020-11-20 16:46:56 +0000

    Begin ApiClient.ts

---
+++ frontend/src/ApiClient.ts
@@ -0,0 +1,27 @@
+﻿import axios from 'axios';
+export class ApiClient {
+    private _baseURL: string;
+    
+    constructor(baseURL : string) {
+        this._baseURL = baseURL;
+    }
+    
+    public url(location : string, params? : {[key : string] : string}) : string {
+        let result = this._baseURL+location;
+        if (params) {
+            result += '?';
+            for (let key of Object.keys(params)) {
+                result += `${key}=${params[key]}&`;
+            }
+        }
+        return result;
+    }
+    
+    public createLobby() {
+        axios.get(this.url('/api/lobby/createLobby.php'))
+            .then(r => {
+                console.log(r);
+            })
+            .catch(alert);
+    }
+}
